1967d521122b30e4410fb03d2c6610ce2538a61f
Tursunbekova Nazmi <nazmitursunbekova@gmail.com>
2023-04-04 16:04:03 +0600

    Added the navigation to the App component

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,14 @@
+import { NavLink } from "react-router-dom";
+
 export default function App(){
   return (
   <div className="App">
     <h1>Hello world</h1>
     <p>This is my diploma</p>
+    <nav>
+      <NavLink to="/">Home</NavLink>
+      <NavLink to="/">About</NavLink>
+    </nav>
   </div>
   );
 }
