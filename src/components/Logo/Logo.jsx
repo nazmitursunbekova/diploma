2be739d95897a3e1766cc2409e7dd43d6248522b
Tursunbekova Nazmi <nazmitursunbekova@gmail.com>
2023-05-04 02:59:31 +0600

    changed the logo

--- src/components/Logo/Logo.jsx
+++ src/components/Logo/Logo.jsx
@@ -1,5 +1,5 @@
 import "./Logo.css";
-import logo from "../../assets/logo.svg";
+import logo from "../../assets/logo.jpg";
 import { NavLink } from "react-router-dom";
 
 export default function Logo(){
