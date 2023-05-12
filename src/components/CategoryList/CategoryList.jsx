b050856d81b0e092dd0572e446c7897a7acc480f
Tursunbekova Nazmi <nazmitursunbekova@gmail.com>
2023-05-12 10:29:45 +0600

    added text with h1 tag

--- src/components/CategoryList/CategoryList.jsx
+++ src/components/CategoryList/CategoryList.jsx
@@ -3,6 +3,8 @@ import"./CategoryList.css";
 import { NavLink } from "react-router-dom";
 import { AppContext } from "../../App";
 import AddCategory from "../AddCategory/AddCategory";
+import DeleteCategory from "../DeleteCategory/Dele/DeleteCategory";
+import Category from "../../pages/Category";
 
 
 export default function CategoryList() {
@@ -11,12 +13,13 @@ export default function CategoryList() {
   const output = categories.map((category) => (
     <li key={category.id}>
       <NavLink to={"/category/" + category.path}>{category.name}</NavLink>
-      
+      <DeleteCategory category={category}/>
     </li>
   ));
 
   return (
     <div className="CategoryList">
+      <h1>POPULAR CATEGORIES</h1>
       <ul>{output}</ul>
       <AddCategory/>
     </div>
