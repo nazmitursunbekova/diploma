54d2afec86f845015458c0776b2ba2f88be8f504
Tursunbekova Nazmi <nazmitursunbekova@gmail.com>
2023-05-12 16:04:54 +0600

    formatted CategoryList

--- src/components/CategoryList/CategoryList.jsx
+++ src/components/CategoryList/CategoryList.jsx
@@ -1,27 +1,32 @@
-import { useContext } from "react";
-import"./CategoryList.css";
 import { NavLink } from "react-router-dom";
+import "./CategoryList.css";
+import { useContext } from "react";
 import { AppContext } from "../../App";
 import AddCategory from "../AddCategory/AddCategory";
 import DeleteCategory from "../DeleteCategory/Dele/DeleteCategory";
-import Category from "../../pages/Category";
 
 
 export default function CategoryList() {
-  const { categories } = useContext(AppContext);
+    const { categories } = useContext(AppContext);
+    categories.sort((a, b) => b.weight - a.weight)
 
-  const output = categories.map((category) => (
-    <li key={category.id}>
-      <NavLink to={"/category/" + category.path}>{category.name}</NavLink>
-      <DeleteCategory category={category}/>
-    </li>
-  ));
+    const output = categories.sort((a, b) => a.weight - b.weight).map(category => (
+        <li key={category.id}>
 
-  return (
-    <div className="CategoryList">
-      <h1>POPULAR CATEGORIES</h1>
-      <ul>{output}</ul>
-      <AddCategory/>
-    </div>
-  );
+            <NavLink className="Category" to={'/category/' + category.path}>
+                {category.name}
+            </NavLink>
+            <DeleteCategory category={category} />
+        </li>
+    ));
+
+    return (
+        <div className="CategoryList">
+            <p>CATEGORY</p>
+            <ul>
+                {output}
+            </ul>
+            <AddCategory />
+        </div>
+    )
 }
