2ebccdffbaf5da95b7d5979561079744b1fe32b8
Tursunbekova Nazmi <nazmitursunbekova@gmail.com>
2023-05-23 15:12:27 +0600

    added a new category

--- src/components/CategoryList/CategoryList.jsx
+++ src/components/CategoryList/CategoryList.jsx
@@ -5,28 +5,26 @@ import { AppContext } from "../../App";
 import AddCategory from "../AddCategory/AddCategory";
 import DeleteCategory from "../DeleteCategory/Dele/DeleteCategory";
 
-
 export default function CategoryList() {
-    const { categories } = useContext(AppContext);
-    categories.sort((a, b) => b.weight - a.weight)
+  const { categories } = useContext(AppContext);
+  categories.sort((a, b) => b.weight - a.weight);
 
-    const output = categories.sort((a, b) => a.weight - b.weight).map(category => (
-        <li key={category.id}>
-
-            <NavLink className="Category" to={'/category/' + category.path}>
-                {category.name}
-            </NavLink>
-            <DeleteCategory category={category} />
-        </li>
+  const output = categories
+    .sort((a, b) => a.weight - b.weight)
+    .map((category) => (
+      <li key={category.id}>
+        <NavLink className="Category" to={"/category/" + category.path}>
+          {category.name}
+        </NavLink>
+        <DeleteCategory category={category} />
+      </li>
     ));
 
-    return (
-        <div className="CategoryList">
-            <p>POPULAR CATEGORY</p>
-            <ul>
-                {output}
-            </ul>
-            <AddCategory />
-        </div>
-    )
+  return (
+    <div className="CategoryList">
+      <p>POPULAR CATEGORY</p>
+      <ul>{output}</ul>
+      <AddCategory />
+    </div>
+  );
 }
