3950b111852ae64aa1ed6ce57c70dac409e33c10
Tursunbekova Nazmi <nazmitursunbekova@gmail.com>
2023-05-25 16:00:48 +0600

    added a new logo

--- src/components/CartLink/CartLink.jsx
+++ src/components/CartLink/CartLink.jsx
@@ -1,6 +1,7 @@
 import { useContext } from "react";
 import { AppContext } from "../../App";
 import { Link } from "react-router-dom";
+import "./CartLink.css";
 
 export default function CartLink() {
   const { cart } = useContext(AppContext);
@@ -10,7 +11,8 @@ export default function CartLink() {
   return (
     <div className="CartLink">
       <Link to="/cart">
-        Cart ({total})
+        <img src="https://i.pinimg.com/564x/f9/94/47/f99447febc2466ecfd4286d2b688990c.jpg" />
+        ({total})
       </Link>
     </div>
   );
