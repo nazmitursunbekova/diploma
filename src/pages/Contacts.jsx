f2232f271ffd863cad510b70be197c17b592b519
Tursunbekova Nazmi <nazmitursunbekova@gmail.com>
2023-05-25 23:50:34 +0600

    Some classes have been renamed

--- src/pages/Contacts.jsx
+++ src/pages/Contacts.jsx
@@ -5,7 +5,7 @@ export default function Contacts() {
     <div className="Contacts">
       <div className="Apple">
       <img src="https://i.pinimg.com/564x/ca/27/57/ca2757320c646f22fa23fd63aa17131b.jpg" />
-      </div>
+     </div>
       <div class="centered">
         <h1>
           HOUSEHOLD <br></br> APPLIANCES <br></br> SHOP
@@ -40,11 +40,7 @@ export default function Contacts() {
           </a>
         </h3>
       </div>
-      <div className="Cook">
-        <img src="https://i.pinimg.com/564x/0d/86/0c/0d860c602c81beb7ffe36c9700f4cc38.jpg" />
-        <img src="https://i.pinimg.com/564x/75/39/bc/7539bc2b1291c96a25e1d9903c8bceac.jpg" />
-        <img src="https://i.pinimg.com/564x/77/5a/ea/775aeaa094dc3809a66a819c35d5b9a8.jpg" />
-      </div>
+      
       <div className="map">
         <iframe
           src="https://yandex.ru/map-widget/v1/?um=constructor%3Acf98b60208e9449827ed9f5c36c0ffdc0045f73467c474bfe98853fc49bd9729&amp;source=constructor"
@@ -54,5 +50,6 @@ export default function Contacts() {
         ></iframe>
       </div>
     </div>
+  
   );
 }
