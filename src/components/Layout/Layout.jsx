aef565a338bffdb2e547773b90f0554838b6fdca
Tursunbekova Nazmi <nazmitursunbekova@gmail.com>
2023-05-25 14:49:23 +0600

    added footer

--- src/components/Layout/Layout.jsx
+++ src/components/Layout/Layout.jsx
@@ -7,6 +7,7 @@ import "./Layout.css";
 import NavToggle from "../NavToggle/NavToggle";
 import Drawer from "../Drawer/Drawer";
 import CartLink from "../CartLink/CartLink";
+import Footer from "../Footer/Footer";
 
 export default function Layout(props) {
   const [drawerOpen, setDrawerOpen] = useState(false);
@@ -23,12 +24,13 @@ export default function Layout(props) {
         <Drawer open={drawerOpen} toggle={toggleDrawer} />
         <CartLink />
           <Auth />
+         
       </header>
       <aside>
         <CategoryList />
       </aside>
       <main>{props.children}</main>
-      <footer>FOOTER</footer>
+      <footer><Footer/></footer>
     </div>
   );
 }
