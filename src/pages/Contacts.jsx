c118ba2e3c1696314ab2ec3e83303369d9f64683
Tursunbekova Nazmi <nazmitursunbekova@gmail.com>
2023-05-25 22:53:49 +0600

    added map and style

--- src/pages/Contacts.jsx
+++ src/pages/Contacts.jsx
@@ -3,17 +3,12 @@ import "./Contacts.css";
 export default function Contacts() {
   return (
     <div className="Contacts">
-           <img src="https://i.pinimg.com/564x/21/ec/bd/21ecbde5ad69055a5874016294faff9f.jpg"/>
-      <div class="centered">HOUSEHOLD APPLIANCES SHOP</div>
-
-      <div className="Techn">
-        <div className="col-6">
- 
-        </div>
-      </div>
-
-      <div className="Tech">
-        <div className="Col-6">
+           <img src="https://i.pinimg.com/564x/ca/27/57/ca2757320c646f22fa23fd63aa17131b.jpg"/>
+           <img src="https://i.pinimg.com/564x/97/ec/6d/97ec6dc4f9456e21d4ce8b61e3b67eee.jpg" alt="" srcset="" />
+      <div class="centered">
+        <h1>HOUSEHOLD  <br></br> APPLIANCES <br></br> SHOP
+        </h1>
+         <h3>
           Our contacts: <br />
           <a
             className="Tel"
@@ -41,18 +36,18 @@ export default function Contacts() {
           >
             +996  708 22 36 18
           </a>
+          </h3>
         </div>
-                        <div className="Col-6">
-                    
-                </div>
-
+         <div className="map">
+        <iframe
+          src="https://yandex.ru/map-widget/v1/?um=constructor%3Acf98b60208e9449827ed9f5c36c0ffdc0045f73467c474bfe98853fc49bd9729&amp;source=constructor"
+          width="100%"
+          height="400"
+          frameborder="0"
+        ></iframe>
       </div>
-      <p>
-        <strong> Our adress : </strong>KYRGYSZTAN BISHKEK st AITMATOV 155A{" "}
-      </p>
-      <p>
-        <strong>Our email : </strong>brand_name@gmail.com{" "}
-      </p>
-    </div>
+        </div>
+  
+    
   );
 }
