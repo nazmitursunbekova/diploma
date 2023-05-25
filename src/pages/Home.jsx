7a29c1d2d702afd716f34dcfc38d7a6b3b687753
Tursunbekova Nazmi <nazmitursunbekova@gmail.com>
2023-05-25 20:45:24 +0600

    picture deleted Home jsx

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -7,7 +7,7 @@ export default function Home() {
       <img src="https://i.pinimg.com/564x/72/03/fa/7203fafbacb23952835d189df951d8a9.jpg" />
       <img src="https://i.pinimg.com/564x/63/95/11/6395113254ad8c09de654648c435073d.jpg" />
       <img src="https://i.pinimg.com/564x/1f/e0/6c/1fe06c2a8ac93a206f1c6cf79270e1e0.jpg" />
-      <img src="https://i.pinimg.com/564x/60/6d/fd/606dfd69e43c596590497fa650b07671.jpg" />
+
 
       <h2>
         The technologies of the future are developments that pursue a specific
@@ -16,18 +16,6 @@ export default function Home() {
         over a medieval canal.
       </h2>
 
-      <h3>
-        Will be soon. In 30-40 years, scientists predict the emergence of even
-        more useful technologies. People will get an HIV vaccine, 3D-printed
-        organs, an exoskeleton that will make them as strong as the Hulk.
-      </h3>
-      <h4>
-        And they will also ride in a high-speed pipe, like in Futurama,
-        contactlessly transfer their energy and data from the brain directly to
-        the Internet. It sounds like the script of a science fiction movie, but
-        this is not fiction, but real developments that are already underway
-        today.
-      </h4>
     </div>
   );
 }
