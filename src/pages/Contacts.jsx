58d0687a2d54f0418a16b26ba8e7c026f70a6cb4
Tursunbekova Nazmi <nazmitursunbekova@gmail.com>
2023-05-25 14:52:41 +0600

    added a new  picture

--- src/pages/Contacts.jsx
+++ src/pages/Contacts.jsx
@@ -5,12 +5,14 @@ export default function Contacts() {
     <div className="Contacts">
       <h1>HOUSEHOLD APPLIANCES SHOP</h1>
       <div className="Techn">
-        <div className="col-3">
+        <div className="col-6">
+ 
+          <img src="https://i.pinimg.com/564x/21/ec/bd/21ecbde5ad69055a5874016294faff9f.jpg"/>
         </div>
       </div>
 
       <div className="Tech">
-        <div className="Col-3">
+        <div className="Col-6">
           Our contacts: <br />
           <a
             className="Tel"
