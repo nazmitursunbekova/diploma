b3be6a8d7ddedf53d58fea3269946b58ebaf72da
Tursunbekova Nazmi <nazmitursunbekova@gmail.com>
2023-05-25 14:51:45 +0600

    formatted

--- src/pages/Delivery.jsx
+++ src/pages/Delivery.jsx
@@ -1,50 +1,39 @@
-import "./Delivery.css"
+import "./Delivery.css";
 
 export default function Delivery() {
   return (
     <div className="Delivery">
-      <h1>Payment and delivery</h1>
-      <h2>ORDERING An order can be placed in several ways.</h2>
-      <div className="Row">
-        <div className="Col-4">
-          <div className="First">
-         
+      <div className="Container">
+        <h1>Payment and delivery</h1>
+        <h2>ORDERING An order can be placed in several ways.</h2>
+        <div className="Row">
+          <div className="Col-6">
             <p>
+              {" "}
               On the site: Select a product and add it to your shopping cart by
-              pressing the "Add to Cart" button next to the
-              product.
+              pressing the "Add to Cart" button next to the product.
             </p>
-          </div>
-        </div>
-        <div className="Col-4">
-          <div className="First">
             <p>
               Go to the shopping cart in the top right corner of the site. Press
               the "Checkout" button. Enter your name, phone number, and delivery
               address (optional address if needed)
             </p>
-          </div>
-        </div>
-        <div className="Col-4">
-          <div className="First">
             <p>
               Select a Payment Method. Choose a shipping method. Press the
               "Confirm order" button and wait for the operator's call (up to 15
               minutes)
             </p>
           </div>
+          <div className="Col-6">
+            <div className="About-me">
+              <img src="https://i.pinimg.com/564x/a0/49/84/a049845050c5e60f407604c88705b8e7.jpg" />
+            </div>
+          </div>
+          </div>
+          <div className="HaveNiceDay"> HAVE A NICE DAYI!</div>
+          <h3>THANKS FOR CHOOSING US!</h3>
         </div>
       </div>
-      <div className="AboutDelivery">
-        <p>
-          Once you have met all three points, you can expect your order <br />
-          Delivery is carried out during the day in the city and during the week
-          in other regions <br />
-          
-        </p>
-        <div className="HaveNiceDay"> HAVE A NICE DAYI!</div>
-        <h3>THANKS FOR CHOOSING US!</h3>
-      </div>
-    </div>
+    
   );
 }
