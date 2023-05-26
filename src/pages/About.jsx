dda13ac74f674b1288f60efb9258078d256ef70a
Tursunbekova Nazmi <nazmitursunbekova@gmail.com>
2023-05-26 13:31:02 +0600

    changed paragraphs About.jsx

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -1,53 +1,65 @@
- import "./About.css";
+import "./About.css";
 
 export default function About() {
   return (
     <div className="About">
       <h1>BRAND NAME</h1>
-      <div className="Cook">
-    
-        <img src="https://i.pinimg.com/564x/0d/86/0c/0d860c602c81beb7ffe36c9700f4cc38.jpg" />
-        <img src="https://i.pinimg.com/564x/75/39/bc/7539bc2b1291c96a25e1d9903c8bceac.jpg" />
-        <img src="https://i.pinimg.com/564x/77/5a/ea/775aeaa094dc3809a66a819c35d5b9a8.jpg" />
-    
-      </div>
-      <div className="Row">
-        <div className="Col-6">
-          <h2>BRAND NAME - is one of the best brands in the world, offering all
-            the necessary equipment for the home</h2>
-          <p>
-         About us
-          <br/>
-            Every 2 seconds, BRAND NAME household appliances are bought
-            somewhere in the world. It is the third largest manufacturer of home
-            appliances in Europe and a global brand that can be found in more
-            than 130 countries. Over 400 million people know and trust our
-            products
-          </p>
-          <h3>Ecology</h3>
-          <p>
-            BRAND NAME cares not only about the convenience, durability and
-            appearance of the produced equipment, but also about environmental
-            protection. Therefore, the production process and products of the
-            plant meet the most stringent environmental standards.
-          </p>
-          <h4>Achievements</h4>
-          <p>
-            Achievements On December 24, 2020, BRAND NAME received two awards in
-            the competition "100 Best Goods of Kyrgyzstan" for 2020! The company
-            took part in the nomination "Industrial goods for the population"
-            and received the following awards: • Title of laureate (Gold sign)
-            for refrigerators; • Diploma title (Silver badge) for washing
-            machines; As well as an additional distinction "New" in
-            refrigerators and washing machines, which is issued to new products
-            in terms of technology and design.
-          </p>
+      <h2>
+        BRAND NAME - is one of the best brands in the world, offering all the
+        necessary equipment for the home
+      </h2>
+
+      <div className="Container">
+        <div className="Row">
+          <div className="Col-6">
+            <p>
+              Every 2 seconds, BRAND NAME household appliances are bought
+              somewhere in the world. It is the third largest manufacturer of
+              home appliances in Europe and a global brand that can be found in
+              more than 130 countries. Over 400 million people know and trust
+              our products
+            </p>
+            <h5>About us</h5>
+
+<p>
+  Every 2 seconds, BRAND NAME household appliances are bought
+  somewhere in the world. It is the third largest manufacturer of home
+  appliances in Europe and a global brand that can be found in more
+  than 130 countries. Over 400 million people know and trust our
+  products
+</p>
+<h3>Ecology</h3>
+<p>
+  BRAND NAME cares not only about the convenience, durability and
+  appearance of the produced equipment, but also about environmental
+  protection. Therefore, the production process and products of the
+  plant meet the most stringent environmental standards.
+</p>
+<h4>Achievements</h4>
+<p>
+  Achievements On December 24, 2020, BRAND NAME received two awards in
+  the competition "100 Best Goods of Kyrgyzstan" for 2020! The company
+  took part in the nomination "Industrial goods for the population"
+  and received the following awards: • Title of laureate (Gold sign)
+  for refrigerators; • Diploma title (Silver badge) for washing
+  machines; As well as an additional distinction "New" in
+  refrigerators and washing machines, which is issued to new products
+  in terms of technology and design.
+</p>
+          </div>
+
+          <div className="Col-6">
+            <div className="About-me">
+              <img src="https://i.pinimg.com/564x/0d/86/0c/0d860c602c81beb7ffe36c9700f4cc38.jpg" />
+              <img src="https://i.pinimg.com/564x/0d/86/0c/0d860c602c81beb7ffe36c9700f4cc38.jpg" />
+              <img src="https://i.pinimg.com/564x/75/39/bc/7539bc2b1291c96a25e1d9903c8bceac.jpg" />
+            </div>
+          </div>
         </div>
-        <div className="Col-6">
-          <div className="About-me"></div>
-        </div>
       </div>
-      <p className="Photo-cosmetics"></p>
-    </div>
+
+      
+      </div>
+
   );
 }
