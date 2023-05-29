3bc52464fe22457f65e7b82df89b1543132f5544
Tursunbekova Nazmi <nazmitursunbekova@gmail.com>
2023-05-29 12:07:05 +0600

    fixed some bugs in maps

--- src/pages/Contacts.jsx
+++ src/pages/Contacts.jsx
@@ -3,9 +3,6 @@ import "./Contacts.css";
 export default function Contacts() {
   return (
     <div className="Contacts">
-      <div className="Apple">
-      <img src="https://i.pinimg.com/564x/ca/27/57/ca2757320c646f22fa23fd63aa17131b.jpg" />
-     </div>
       <div class="centered">
         <h1>
           HOUSEHOLD <br></br> APPLIANCES <br></br> SHOP
@@ -41,7 +38,9 @@ export default function Contacts() {
           </a>
         </h3>
       </div>
-      
+      <div className="Apple">
+        <img src="https://i.pinimg.com/564x/ca/27/57/ca2757320c646f22fa23fd63aa17131b.jpg" />
+      </div>
       <div className="map">
         <iframe
           src="https://yandex.ru/map-widget/v1/?um=constructor%3Acf98b60208e9449827ed9f5c36c0ffdc0045f73467c474bfe98853fc49bd9729&amp;source=constructor"
