eccf57867984322ee3b59cd5df75c01af06ffbe5
Tursunbekova Nazmi <nazmitursunbekova@gmail.com>
2023-05-29 12:11:21 +0600

    added class name and text

--- src/pages/Product.jsx
+++ src/pages/Product.jsx
@@ -17,9 +17,16 @@ export default function Product() {
   return (
     <div className="Product">
       <h1>{product.name}</h1>
-      <img src={product.picture} alt={product.name} />
-      <span>{product.price} som</span>
-      <AddToCart product={product} />
+      <div className="Product_in">
+        <div className="Product_img">
+          <img src={product.picture} alt={product.name} />
+        </div>
+        <div className="Product_content">
+          <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Facere perferendis laudantium eos incidunt laborum labore id vero neque soluta sequi asperiores quae impedit, voluptatibus animi excepturi ipsam sit minus officiis veniam. Esse, nemo itaque officiis asperiores accusamus praesentium natus porro quam laudantium eveniet magnam est incidunt fugit architecto odio ex, illum possimus iusto sapiente soluta libero beatae. Aut eveniet optio earum similique nemo deleniti nihil distinctio, ea neque dolore, voluptas error rerum dolorem nostrum animi, odit praesentium provident cum illo!</p>
+          <span>{product.price} som</span>
+          <AddToCart product={product} />
+        </div>
+      </div>
     </div>
   );
 }
