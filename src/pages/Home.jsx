cf2f6e265e8b5087ee931f0f2c0dee7594b9d2ee
Tursunbekova Nazmi <nazmitursunbekova@gmail.com>
2023-05-29 09:35:13 +0600

    formatted pages Home.jsx

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -5,8 +5,9 @@ export default function Home() {
       <h1>TECHNOLOGY AND INNAVATION</h1>
 
     <div className="home_imgs">
-      <div className="home_img"><img src="https://i.pinimg.com/564x/43/83/a7/4383a7c5d99340c89ecef408f3b37c14.jpg"  /></div>
+      <div className="home_img"></div>
       <div className="home_img"><img src="https://i.pinimg.com/564x/84/e0/54/84e0540a22628568682036c616897b99.jpg" alt="" srcset="" /></div>
+      <div className="home_img"><img src="https://i.pinimg.com/564x/63/95/11/6395113254ad8c09de654648c435073d.jpg" /></div>
       <div className="home_img"><img src="https://i.pinimg.com/564x/63/95/11/6395113254ad8c09de654648c435073d.jpg" /></div>
     </div>
       
@@ -20,6 +21,10 @@ export default function Home() {
         transmit data faster than the wheels turn. This is a 3D printed bridge
         over a medieval canal.
       </h2>
+
+
+
+
 
 
     </div>
