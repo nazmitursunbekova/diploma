f48ed7771780118e03b9574174240d843c8cee3d
Tursunbekova Nazmi <nazmitursunbekova@gmail.com>
2023-05-29 09:30:11 +0600

    formatted CartLink.jsx

--- src/components/CartLink/CartLink.jsx
+++ src/components/CartLink/CartLink.jsx
@@ -11,8 +11,7 @@ export default function CartLink() {
   return (
     <div className="CartLink">
       <Link to="/cart">
-        <img src="https://i.pinimg.com/564x/f9/94/47/f99447febc2466ecfd4286d2b688990c.jpg" />
-        ({total})
+Sign in({total})
       </Link>
     </div>
   );
