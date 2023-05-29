2cf1a064cee65c476edafb272c27cd7323fb558f
Tursunbekova Nazmi <nazmitursunbekova@gmail.com>
2023-05-29 09:32:54 +0600

    formatted

--- src/components/CategoryList/CategoryList.jsx
+++ src/components/CategoryList/CategoryList.jsx
@@ -3,7 +3,8 @@ import "./CategoryList.css";
 import { useContext } from "react";
 import { AppContext } from "../../App";
 import AddCategory from "../AddCategory/AddCategory";
-import DeleteCategory from "../DeleteCategory/Dele/DeleteCategory";
+import DeleteCategory from "../DeleteCategory/DeleteCategory";
+
 
 export default function CategoryList() {
   const { categories } = useContext(AppContext);
