d3af60300dd0c44786f7f1aeb4b397f906bbdd27
Tursunbekova Nazmi <nazmitursunbekova@gmail.com>
2023-05-29 09:31:53 +0600

    added import "./CartList.css";

--- src/components/CartList/CartList.jsx
+++ src/components/CartList/CartList.jsx
@@ -2,19 +2,47 @@ import { useContext } from "react";
 import { AppContext } from "../../App";
 import { Link } from "react-router-dom";
 import "./CartList.css";
+
+
 export default function CartList() {
+  // получить продукты и содердижимое корзины
   const { products, cart, setCart } = useContext(AppContext);
+
+  function onQtyChange(product, qty) {
+    setCart({
+      ...cart,
+      [product.id]: qty,
+    });
+  }
+
+  function onRemoveClick(product) {
+    const newCart = { ...cart };
+    delete newCart[product.id];
+    setCart(newCart);
+  }
 
   const productIds = Object.keys(cart);
   const output = products
     .filter((product) => productIds.includes(product.id))
     .map((product) => (
       <div className="CartItem">
-        <img src={product.picture} alt={product.name} />
-        <Link to={"/product/" + product.path}>{product.name}</Link>
-
-        <span>{cart[product.id]} items</span>
-        <span>{product.price * cart[product.id]} som</span>
+        <img
+          className="Product_picture"
+          src={product.picture}
+          alt={product.name}
+        />
+        <Link className="Product_name" to={"/product/" + product.path}>
+          {product.name}
+        </Link>
+        <input
+          type="number"
+          min={1}
+          onChange={(event) => onQtyChange(product, +event.target.value)}
+          value={cart[product.id]}
+        />
+        <span className="Product_price">
+          {product.price * cart[product.id]} som{" "}
+        </span>
       </div>
     ));
 
